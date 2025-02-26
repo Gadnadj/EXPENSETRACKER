f91848f4cb21eef35fff358ce0f636830f1d6d7b
Gad Nadjar <gadnadjar@gmail.com>
2025-02-26 17:54:26 +0200

    finish with user routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import path from 'path';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 dotenv.config();
 const app = express();
@@ -20,6 +22,12 @@ app.use(express.json());
 connectDB();
 
 app.use('/api/v1/auth', authRoutes);
+
+//serve uploads folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
