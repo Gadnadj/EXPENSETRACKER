596610e77bb5b93b8a011d44123369d9ba172672
Gad Nadjar <gadnadjar@gmail.com>
2025-02-26 16:44:09 +0200

    add backend

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { registerUser, loginUser, getUserInfo } from '../controllers/authController.js'
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+
+router.post('login', loginUser);
+
+router.get('./getUser', getUserInfo);
+
+module.exports = router;
