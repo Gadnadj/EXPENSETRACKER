2cd11525e7d54a8494d24072898e83b8d8c0eaf6
Gad Nadjar <gadnadjar@gmail.com>
2025-02-28 14:45:50 +0200

    add label

--- frontend/expense-tracker/src/components/Charts/CustomPieChart.tsx
+++ frontend/expense-tracker/src/components/Charts/CustomPieChart.tsx
@@ -12,57 +12,38 @@ type Props = {
 
 const CustomPieChart = ({ data, label, totalAmount, colors, showTextAnchor }: Props) => {
     return (
-        <ResponsiveContainer width='100%' height={380}>
-            <PieChart>
-                <Pie
-                    data={data}
-                    dataKey='amount'
-                    nameKey='name'
-                    cx='50%'
-                    cy='50%'
-                    outerRadius={130}
-                    innerRadius={100}
-                    labelLine={false}
-                >
-                    {data.map((_, index) => (
-                        <Cell
-                            key={`cell-${index}`}
-                            fill={colors[index % colors.length]}
-                        />
-                    ))}
-                </Pie>
-                <Tooltip content={CustomTooltip} />
-                <Legend content={CustomLegend} />
+        <div className="relative">
+            <ResponsiveContainer width='100%' height={380}>
+                <PieChart>
+                    <Pie
+                        data={data}
+                        dataKey='amount'
+                        nameKey='name'
+                        cx='50%'
+                        cy='50%'
+                        outerRadius={130}
+                        innerRadius={100}
+                        labelLine={false}
+                    >
+                        {data.map((_, index) => (
+                            <Cell
+                                key={`cell-${index}`}
+                                fill={colors[index % colors.length]}
+                            />
+                        ))}
+                    </Pie>
+                    <Tooltip content={CustomTooltip} />
+                    <Legend content={CustomLegend} />
+                </PieChart>
+            </ResponsiveContainer>
 
-                {
-                    showTextAnchor && (
-                        <>
-                            <text
-                                x='50%'
-                                y='50%'
-                                dy={-25}
-                                textAnchor='middle'
-                                fill='#666'
-                                fontSize='14px'
-                            >
-                                {label}
-                            </text>
-                            <text
-                                x='50%'
-                                y='50%'
-                                dy={8}
-                                textAnchor='middle'
-                                fill='#333'
-                                fontSize='24px'
-                                fontWeight='600'
-                            >
-                                {totalAmount}
-                            </text>
-                        </>
-                    )
-                }
-            </PieChart>
-        </ResponsiveContainer>
+            {showTextAnchor && (
+                <div className="absolute flex flex-col items-center justify-center top-[175px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+                    <p className="text-gray-600 text-sm mb-1">{label}</p>
+                    <p className="text-2xl font-semibold text-gray-800">{totalAmount}</p>
+                </div>
+            )}
+        </div>
     );
 };
 
