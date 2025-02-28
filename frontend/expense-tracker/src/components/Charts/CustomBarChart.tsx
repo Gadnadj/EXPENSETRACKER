4a8a589f594223681945c6a15ca856df927e7355
Gad Nadjar <gadnadjar@gmail.com>
2025-02-28 15:23:27 +0200

    complete 30 days expenses

--- frontend/expense-tracker/src/components/Charts/CustomBarChart.tsx
+++ frontend/expense-tracker/src/components/Charts/CustomBarChart.tsx
@@ -1,13 +1,55 @@
-import React from 'react';
-
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Cell } from 'recharts';
 type Props = {
     data: { category: string, amount: number }[]
 }
 
 const CustomBarChart = ({ data }: Props) => {
+
+    const getBarColor = (index: number) => {
+        return index % 2 === 0 ? '#875cf5' : '#cfbefb';
+    };
+
+    const CustomTooltip = ({ active, payload }: { active?: boolean, payload?: any[] }) => {
+        if (active && payload && payload.length) {
+            return (
+                <div className='bg-white shadow-md rounded-lg p-2 border border-gray-300'>
+                    <p className='text-xs font-semibold text-purple-800 mb-1'>
+                        {payload[0].payload.category}
+                    </p>
+                    <p className='text-sm text-gray-600'>
+                        Amount: <span className='text-sm font-medium text-gray-900'>${payload[0].payload.amount}</span>
+                    </p>
+                </div>
+            );
+        }
+        return null;
+    };
     return (
-        <div>
-            
+        <div className='bg-white mt-6'>
+            <ResponsiveContainer
+                width='100%'
+                height={300}
+            >
+                <BarChart data={data}>
+                    <CartesianGrid stroke='none' />
+
+                    <XAxis dataKey='month' tick={{ fontSize: 12, fill: '#555' }} stroke='none' />
+                    <YAxis tick={{ fontSize: 12, fill: '#555' }} stroke='none' />
+                    <Tooltip content={CustomTooltip} />
+
+                    <Bar
+                        dataKey='amount'
+                        fill='#FF8042'
+                        // radius={[10, 10, 0, 0]}
+                        // activeDot={{ r: 8, fill: 'yellow' }}
+                        // activeStyle={{ fill: 'green' }}
+                    >
+                        {data.map((_, index) => (
+                            <Cell key={index} fill={getBarColor(index)} />
+                        ))}
+                    </Bar>
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
