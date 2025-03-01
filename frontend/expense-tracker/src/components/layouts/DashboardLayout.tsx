ec86563b8e4f7adaea886ddcebc565351e51574b
Gad Nadjar <gadnadjar@gmail.com>
2025-03-01 15:35:11 +0200

    fix bug

--- frontend/expense-tracker/src/components/layouts/DashboardLayout.tsx
+++ frontend/expense-tracker/src/components/layouts/DashboardLayout.tsx
@@ -17,16 +17,16 @@ const DashboardLayout = ({ children, activeMenu }: Props) => {
         <div className={`min-h-screen flex flex-col transition-colors duration-300 ${
             isDarkMode ? 'bg-gray-900' : 'bg-gray-50'
         }`}>
-            <div className="fixed top-0 left-0 right-0 z-50">
+            <div className="fixed top-0 left-0 right-0 z-50 safe-top">
                 <Navbar activeMenu={activeMenu} />
             </div>
             {user && (
-                <div className='flex mt-[73px]'>
-                    <div className='max-[1080px]:hidden'>
+                <div className='flex mt-[73px] pt-safe-top'>
+                    <div className='max-[1080px]:hidden sticky top-[73px] h-[calc(100vh-73px)]'>
                         <SideMenu activeMenu={activeMenu} />
                     </div>
 
-                    <div className='grow mx-5'>
+                    <div className='grow mx-5 pb-6'>
                         {children}
                     </div>
                 </div>
--- frontend/expense-tracker/src/main.tsx
+++ frontend/expense-tracker/src/main.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ThemeProvider } from './context/ThemeContext.tsx';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import './styles/pwa.css';
+import { register } from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
-// Si vous voulez que votre application fonctionne hors ligne
-// et se charge plus rapidement, vous pouvez changer unregister() en register() ci-dessous.
-serviceWorkerRegistration.register();
+register();
