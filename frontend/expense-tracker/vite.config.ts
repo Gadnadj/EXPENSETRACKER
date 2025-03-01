bae0d7863c66c4a542d3e8e02676d871ddc15f28
Gad Nadjar <gadnadjar@gmail.com>
2025-03-01 14:58:17 +0200

    fix

--- frontend/expense-tracker/vite.config.ts
+++ frontend/expense-tracker/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,13 +8,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        'service-worker': resolve(__dirname, 'src/service-worker.ts')
-      },
-      output: {
-        entryFileNames: (assetInfo) => {
-          return assetInfo.name === 'service-worker' ? '[name].js' : 'assets/js/[name]-[hash].js';
-        }
+        app: './index.html',
+        'service-worker': './src/service-worker.ts'
       }
     }
   },
